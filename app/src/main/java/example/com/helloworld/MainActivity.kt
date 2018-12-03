588d450f23a76e75b3455493b6cd9b74dcd05abe
Rana Ranvijay Singh <rana.singh@globant.com>
2018-12-03 17:27:31 +0530

    Commit test 3

--- app/src/main/java/example/com/helloworld/MainActivity.kt
+++ app/src/main/java/example/com/helloworld/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("Main","Test 1")
+        Log.d("Main","Test 3")
     }
 }
