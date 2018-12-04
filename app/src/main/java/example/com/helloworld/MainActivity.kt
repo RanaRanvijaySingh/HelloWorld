35b7f6a0d5cacec8046074eee8c23d62a09dc372
Rana Ranvijay Singh <rana.singh@globant.com>
2018-12-04 17:36:02 +0530

    Commit test 12

--- app/src/main/java/example/com/helloworld/MainActivity.kt
+++ app/src/main/java/example/com/helloworld/MainActivity.kt
@@ -3,26 +3,12 @@ package example.com.helloworld
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.KeyEvent
-import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("Main", "Test 11")
-        initView()
-    }
-
-    private fun initView() {
-        et.setOnKeyListener { _, keyCode, _ ->
-            if (keyCode == KeyEvent.KEYCODE_DEL) {
-                Log.i("Key", "${count++}")
-            }
-            false
-        }
+        Log.d("Main", "Test 12")
     }
 }
