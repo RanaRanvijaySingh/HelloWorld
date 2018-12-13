473a903e4a154140712c5ee7e37f4ca34e77f7d5
Rana Ranvijay Singh <rana.singh@globant.com>
2018-12-13 14:53:50 +0530

    Fix failing test case.

--- app/src/test/java/example/com/helloworld/ValidTest.java
+++ app/src/test/java/example/com/helloworld/ValidTest.java
@@ -7,7 +7,7 @@ public class ValidTest {
 
     @Test
     public void emailTestForValidInput(){
-        Assert.assertEquals(false, Valid.email("a@a.com"));
+        Assert.assertEquals(true, Valid.email("a@a.com"));
     }
 
     @Test
