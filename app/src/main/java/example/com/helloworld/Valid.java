d452803301f51bb99131f0f91b3f0e7598be1c18
Rana Ranvijay Singh <rana.singh@globant.com>
2018-12-17 14:38:26 +0530

    Add private constructor for Valid class

--- app/src/main/java/example/com/helloworld/Valid.java
+++ app/src/main/java/example/com/helloworld/Valid.java
@@ -2,10 +2,10 @@ package example.com.helloworld;
 
 public class Valid {
 
+    private Valid() {
+    }
+
     public static boolean email(String email) {
-        return email != null
-                && email.length() >= 5
-                && email.contains("@")
-                && email.contains(".");
+        return email != null && email.length() >= 5 && email.contains("@") && email.contains(".");
     }
 }
